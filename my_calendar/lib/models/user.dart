72b8535e38e12eb0cf1519436bf0857dfaa39a1d
brais0tero <32261639+brais0tero@users.noreply.github.com>
2022-09-06 19:24:56 +0200

    firbase improvement

--- my_calendar/lib/models/user.dart
+++ my_calendar/lib/models/user.dart
@@ -1,19 +1,7 @@
 class User {
-  final int id;
-  final String pass;
-  final String name;
+  final String uid;
 
-  User(this.id, this.name, this.pass);
-
-  User.fromJson(Map<String, dynamic> json)
-      : id = json['id'], name = json['name'], pass = json['pass']
-       ;
-
-  Map<String, dynamic> toJson() => {
-       'id':id,
-        'name': name,
-        'pass' : pass
-      };
-       @override
-  String toString() => name;
+  User({
+    required this.uid
+  });
 }
---
+++ my_calendar/lib/services/auth_service.dart
@@ -0,0 +1,70 @@
+// ignore_for_file: unnecessary_null_comparison
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:my_calendar/helper/helper_functions.dart';
+import 'package:my_calendar/models/user.dart' as localUser;
+import 'package:my_calendar/services/database_service.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+
+  // create user object based on FirebaseUser
+  localUser.User? _userFromFirebaseUser(User user) {
+    return (user != null) ? localUser.User(uid: user.uid) : null;
+  }
+
+
+  // sign in with email and password
+  Future signInWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential result = await _auth.signInWithEmailAndPassword(email: email, password: password);
+      User? user = result.user;
+      return _userFromFirebaseUser(user!);
+    } catch(e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+
+  // register with email and password
+  Future registerWithEmailAndPassword(String fullName, String email, String password) async {
+    try {
+      UserCredential result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      User? user = result.user;
+
+      // Create a new document for the user with uid
+      await DatabaseService(uid: user!.uid).updateUserData(fullName, email, password);
+      return _userFromFirebaseUser(user);
+    } catch(e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  //sign out
+  Future signOut() async {
+    try {
+      await HelperFunctions.saveUserLoggedInSharedPreference(false);
+      await HelperFunctions.saveUserEmailSharedPreference('');
+      await HelperFunctions.saveUserNameSharedPreference('');
+
+      return await _auth.signOut().whenComplete(() async {
+        print("Logged out");
+        await HelperFunctions.getUserLoggedInSharedPreference().then((value) {
+          print("Logged in: $value");
+        });
+        await HelperFunctions.getUserEmailSharedPreference().then((value) {
+          print("Email: $value");
+        });
+        await HelperFunctions.getUserNameSharedPreference().then((value) {
+          print("Full Name: $value");
+        });
+      });
+    } catch(e) {
+      print(e.toString());
+      return null;
+    }
+  }
+}
